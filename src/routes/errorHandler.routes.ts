a093c9e078b9758a770efedd8ddb9362cc7d5abc
harduim300 <harduim300@gmail.com>
2025-03-31 15:24:42 -0300

    trying to fix vercel error

--- src/routes/errorHandler.routes.ts
+++ src/routes/errorHandler.routes.ts
@@ -1,9 +1,15 @@
 import { ErrorRequestHandler, RequestHandler } from "express"
 
 export const NotFoundRequest: RequestHandler = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://gerenciador-tarefas-frontend-one.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(404).json({error: "Route not found"})
 } 
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://gerenciador-tarefas-frontend-one.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(500).json({error: "Ocorreu um erro"})
 }
