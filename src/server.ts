e2cd330b2a6e326a373fd9e64c1afea097586d76
harduim300 <harduim300@gmail.com>
2025-03-30 21:19:01 -0300

    fix: removed cors

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-    origin: [ 'https://frontend-project-watch-d1uhftn56-matheus-harduims-projects.vercel.app'],
+    origin: [ 'https://frontend-project-watch.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
