a4d1a5d182a108654342500fb09702f026ee5666
harduim300 <harduim300@gmail.com>
2025-03-30 22:00:30 -0300

    try fix cors

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json())
-app.use('*', cors())
+app.use(cors({origin: ['https://frontend-project-watch.vercel.app'], credentials: true}))
 app.use('/', router);
 
 app.use(NotFoundRequest)
