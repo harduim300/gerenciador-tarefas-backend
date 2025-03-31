f7697323208651a9beba6d58d75d59ccfe305bea
harduim300 <harduim300@gmail.com>
2025-03-30 21:42:33 -0300

    removed options

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors({
     origin: [ 'https://frontend-project-watch.vercel.app'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use('/', router);
