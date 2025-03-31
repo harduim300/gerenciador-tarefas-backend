3c80cc2a29fe6794c19de44c2447a38b3b51c4d7
harduim300 <harduim300@gmail.com>
2025-03-30 21:08:12 -0300

    fixed: error cors

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [ 'https://frontend-project-watch-d1uhftn56-matheus-harduims-projects.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
