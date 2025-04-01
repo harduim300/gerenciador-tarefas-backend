9fd3373d7ecd76757740d1e86b8bb4a876f49318
harduim300 <harduim300@gmail.com>
2025-04-01 15:05:01 -0300

    teste cors v0.3

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,8 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 'http://localhost:5173');
     res.header("Access-Control-Allow-Methods", "GET,DELETE,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
+    res.header("Access-Control-Allow-Credentials", "true");
+
     if (req.method === "OPTIONS") {
       res.status(200).end();
       return;
