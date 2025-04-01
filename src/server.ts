2b220f7872fb33b5d99b02c10dc3dd4af0433010
harduim300 <harduim300@gmail.com>
2025-04-01 10:03:45 -0300

    test a new solution for cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // 🔹 Proteção extra
 app.use(helmet());
