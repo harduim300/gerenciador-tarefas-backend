652f925030f4450d48f80a7b3a3fa0f0b7cf687a
harduim300 <harduim300@gmail.com>
2025-04-02 08:46:18 -0300

    adding cors function

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,10 @@ import router from "./routes";
 dotenv.config();
 
 const app = express();
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'https://gerenciador-tarefas-frontend-one.vercel.app');
-    res.header("Access-Control-Allow-Methods", "GET,DELETE,POST,PUT,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    
-    if (req.method === "OPTIONS") {
-        res.status(200).end();
-        return;
-    }
-    
-    next();
-});
+app.use(cors({
+    origin: "https://gerenciador-tarefas-frontend-one.vercel.app",
+    credentials: true
+}))
 app.get("/", (req, res) => {
     res.send("Seja bem vindo ao Gerenciador de Tarefas");
 });
